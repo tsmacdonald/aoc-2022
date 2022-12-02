0ac596c9099d8bc5646fc4847a1e11dbb8606cb9
Tim Macdonald <tim@tsmacdonald.com>
2022-12-02 17:07:42 +0000

    Day 2 part 1

--- src/main.lisp
+++ src/main.lisp
@@ -2,6 +2,11 @@
   (:use :cl))
 (in-package :aoc-2022)
 
+;; Util
+(defun slurp-lines (filepath)
+  (with-open-file (in filepath)
+    (loop for l = (read-line in nil) while l collecting l)))
+
 ;; Day 1
 (defun max-calories (provisions)
   (let ((total 0)
@@ -36,3 +41,31 @@
 	     (foods->calorie-counts
 	      (loop for l = (read-line in nil)
 		    while l collecting (parse-integer l :junk-allowed t)))))))
+
+;; Day 2
+(defparameter +winners+ '((rock . scissors) (scissors . paper) (paper . rock)))
+
+(defun winner-score (opp-move your-move)
+  (if (eql opp-move your-move)
+      3
+      (if (member (cons opp-move your-move) +winners+ :test #'equal)
+	  0
+	  6)))
+
+(defparameter +move->score+ '((rock . 1) (paper . 2) (scissors . 3)))
+(defparameter +your-move->move+ '(("X" . rock) ("Y" . paper) ("Z" . scissors)))
+(defparameter +opp-move->move+ '(("A" . rock) ("B" . paper) ("C" . scissors)))
+
+(defun calculate-score (moves)
+  (let ((opp-move  (cdr (assoc (first moves) +opp-move->move+ :test #'string=)))
+	(your-move (cdr (assoc (second moves) +your-move->move+ :test #'string=))))
+    (+ (cdr (assoc your-move +move->score+ :test #'eql))
+       (winner-score opp-move your-move))))
+
+(defun line->moves (line)
+  (str:split " " line))
+
+(defun day-2 ()
+  (reduce #'+
+	  (mapcar #'calculate-score
+	       (mapcar #'line->moves (slurp-lines "../resources/day-2")))))
